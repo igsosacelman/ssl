eff3dffae28682d9bc2a9306854af54a69cc9c53
Ignacio Sosa <ignacio.sosa@wolox.com.ar>
2019-08-26 23:34:45 -0300

    Save scrapped web in .txt

--- SSL-TP1-web-scraping/scrapingWeb.c
+++ SSL-TP1-web-scraping/scrapingWeb.c
@@ -7,10 +7,25 @@ int main(void){
 
     /* wget -q = silent mode */
     FILE *cmd = popen("wget -q -O - http://52.67.191.9/SSL/lideres-bcba.html --no-check-certificate","r");
+    FILE* pFileScrappedWeb;
     char result[1048576]; // 1 MB de buffer, ajustar segun implementacion
-
+    /*
     while (fgets(result,sizeof(result),cmd) != NULL)
         printf("%s", result);
+    */
+    pFileScrappedWeb = fopen("scrappedWeb.txt","w");
+    
+    if(pFileScrappedWeb == NULL) {
+        printf("Error al intentar abrir el archivo.\n");
+        return -1;
+    }
+
+    while (fgets(result,sizeof(result),cmd) != NULL) {
+        fprintf(pFileScrappedWeb,"%s",result);
+    }
+
+    fclose(pFileScrappedWeb);
     pclose(cmd);
+    
     return 0;
 }
