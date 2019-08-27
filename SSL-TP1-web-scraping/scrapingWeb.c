b2a12a71d9f480874f33a8d85cbe73a33490f621
Ignacio Sosa <ignacio.sosa@wolox.com.ar>
2019-08-26 23:56:26 -0300

    Primeros pasos - Obtener html de un link y guardarlo en un archivo

--- SSL-TP1-web-scraping/scrapingWeb.c
+++ SSL-TP1-web-scraping/scrapingWeb.c
@@ -2,30 +2,33 @@
 
 FILE *popen(const char * command, const char *mode);
 int pclose(FILE *stream);
+void saveScrappedWebInFile(FILE* read,FILE* write,char* buffer);
+void closeFilePointer(FILE* read,FILE* write);
 
 int main(void){
 
-    /* wget -q = silent mode */
     FILE *cmd = popen("wget -q -O - http://52.67.191.9/SSL/lideres-bcba.html --no-check-certificate","r");
-    FILE* pFileScrappedWeb;
+    FILE *pFileScrappedWeb = fopen("scrappedWeb.txt","w");
     char result[1048576]; // 1 MB de buffer, ajustar segun implementacion
-    /*
-    while (fgets(result,sizeof(result),cmd) != NULL)
-        printf("%s", result);
-    */
-    pFileScrappedWeb = fopen("scrappedWeb.txt","w");
     
-    if(pFileScrappedWeb == NULL) {
+    if(cmd == NULL || pFileScrappedWeb == NULL) {
         printf("Error al intentar abrir el archivo.\n");
-        return -1;
+        return 1;
     }
 
-    while (fgets(result,sizeof(result),cmd) != NULL) {
-        fprintf(pFileScrappedWeb,"%s",result);
-    }
+    saveScrappedWebInFile(cmd,pFileScrappedWeb,result);
+    closeFilePointer(cmd,pFileScrappedWeb);
 
-    fclose(pFileScrappedWeb);
-    pclose(cmd);
-    
     return 0;
+}
+
+void saveScrappedWebInFile(FILE* read,FILE* write,char* buffer){
+    while (fgets(buffer,sizeof(buffer),read) != NULL) {
+        fprintf(write,"%s",buffer);
+    }
+}
+
+void closeFilePointer(FILE* read,FILE* write){
+    fclose(read);
+    pclose(write);
 }
